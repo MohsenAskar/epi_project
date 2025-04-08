b085d457e6a62af66d1b153876895779c6070b6d
MohsenAskar <71376243+MohsenAskar@users.noreply.github.com>
2025-04-08 16:49:52 +0200

    edits2

--- pages/0_Definitions.py
+++ pages/0_Definitions.py
@@ -6,24 +6,61 @@ st.title("Important Definitions")
 st.write("""
 The definitions are sorted after the different modules
 - **Data Distributions module**
-    -
+    - Mean:
+    - Standard Deviation (SD):
+    - Sample Size (n):
+    - Cumulative Distribution Function (CDF):
+    - Weibull Distribution: 
+    - Statistical power: 
+    - p-value (alpha (α)):
+    - Exceedance Probability (EP):
+    - Arithmetic Mean: 
+    
+    
 - **Regression module**
-    -
+    - Dependent variable (Y):
+    - Independent variable (X):
+    - y-intercept:
+    - coefficient:
+    - The slope coefficient:
+    - Error term (residual):
+    - R-squared (R2): 
+    - Fit line (Regression line):
+    - R² (Coefficient of Determination):
+    - RMSE (Root Mean Square Error):
+    - Binary outcome:
+    - Sigmoid function:
+    - AUC (Area Under ROC Curve):
+    - The odds ratio (OR):
     
 - **Correlation module**
-    -
+    - Continuous variables:
+    - Categorical variables:
 - **Causal Inference module**
-    -
+    - Propensity Score: 
+    - Instrumental Variables:
+    
 - **Confounding module**
-    -
+    - Crude Effect:  
+    - Adjusted Effect:
+    
+    
 - **Stratification module**
-    -
+    - Effect modification:
+    - Interaction:
+    
 - **Measures of Association module**
-    -
+    - Risk-Based Measures:
+    - Odds-Based Measures:
+    - Incidence: 
+    - Incidence Proportion:
+    
 - **Effect Modification module**
-    -
+    - Statistically significant:
+    
 - **Epidemiological Study Designs module**
-    -
+    - Cohort:
+    
 - **Selection Bias module**
     -
 """)
